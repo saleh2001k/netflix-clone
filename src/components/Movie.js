242154328dff912fdb615f710e7130c0a2292613
saleh2001k <saleh.ayman2001@gmail.com>
2023-05-22 16:55:34 +0300

    fixing .env

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import ModalMovie from './ModalMovie';
+import React, { useState } from "react";
+import axios from "axios";
+import ModalMovie from "./ModalMovie";
 
 const Movie = ({ movie }) => {
   const [showModal, setShowModal] = useState(false);
-  const [comment, setComment] = useState('');
+  const [comment, setComment] = useState("");
 
   const handleAddToFavorites = async () => {
     try {
-      const response = await axios.post('http://localhost:3000/addMovie', {
+      const response = await axios.post(process.env.REACT_APP_API_ADD_MOVIE, {
         title: movie.title,
         release_date: movie.release_date,
         poster_path: movie.poster_path,
         overview: movie.overview,
-        comments: '',
+        comments: "",
       });
 
-      console.log('Movie added to favorites:', response.data);
+      console.log("Movie added to favorites:", response.data);
     } catch (error) {
-      console.log('Error adding movie to favorites:', error);
+      console.log("Error adding movie to favorites:", error);
     }
   };
 
@@ -32,35 +32,34 @@ const Movie = ({ movie }) => {
 
   return (
     <div className="container">
-   <div className="card-container">
-  
-    <div className="card">
-      <img
-        className="card-img-top img-fluid img-thumbnail" 
-        src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-        alt={movie.title || movie.name}
-      />
-      <div className="card-body">
-        <h5 className="card-title">{movie.title || movie.name}</h5>
-        <button className="btn btn-primary" onClick={handleAddToFavorites}>
-          Add to Favorites
-        </button>
-        <button className="btn btn-secondary" onClick={handleShowModal}>
-          Details
-        </button>
+      <div className="card-container">
+        <div className="card">
+          <img
+            className="card-img-top img-fluid img-thumbnail"
+            src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+            alt={movie.title || movie.name}
+          />
+          <div className="card-body">
+            <h5 className="card-title">{movie.title || movie.name}</h5>
+            <button className="btn btn-primary" onClick={handleAddToFavorites}>
+              Add to Favorites
+            </button>
+            <button className="btn btn-secondary" onClick={handleShowModal}>
+              Details
+            </button>
+          </div>
+          {showModal && (
+            <ModalMovie
+              movie={movie}
+              comment={comment}
+              setComment={setComment}
+              onClose={handleCloseModal}
+              onAddToFavorites={handleAddToFavorites}
+            />
+          )}
+        </div>
       </div>
-      {showModal && (
-        <ModalMovie
-          movie={movie}
-          comment={comment}
-          setComment={setComment}
-          onClose={handleCloseModal}
-          onAddToFavorites={handleAddToFavorites}
-        />
-      )}
     </div>
-  </div>
-</div>
   );
 };
 
